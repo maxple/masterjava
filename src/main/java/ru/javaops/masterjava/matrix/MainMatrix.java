e6d21133184f692636860b747801b63f05eb380f
SBT-Pletnev-ML <mlpletnev.sbt@sberbank.ru>
2017-10-18 16:54:51 +0300

    hw1

--- src/main/java/ru/javaops/masterjava/matrix/MainMatrix.java
+++ src/main/java/ru/javaops/masterjava/matrix/MainMatrix.java
@@ -1,8 +1,13 @@
 package ru.javaops.masterjava.matrix;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
+import static java.lang.System.currentTimeMillis;
 
 /**
  * gkislin
@@ -18,35 +23,100 @@ public class MainMatrix {
         final int[][] matrixA = MatrixUtil.create(MATRIX_SIZE);
         final int[][] matrixB = MatrixUtil.create(MATRIX_SIZE);
 
+        final int optPageSize = new MainMatrix().getOptPageSize(matrixA, matrixB);
+        System.out.println("optPageSize=" + optPageSize);
+
         double singleThreadSum = 0.;
+        double singleThreadOptimizedSum = 0.;
         double concurrentThreadSum = 0.;
-        int count = 1;
-        while (count < 6) {
+        int count = 0;
+        while (count < 5) {
+            count++;
             System.out.println("Pass " + count);
-            long start = System.currentTimeMillis();
+            long start = currentTimeMillis();
             final int[][] matrixC = MatrixUtil.singleThreadMultiply(matrixA, matrixB);
-            double duration = (System.currentTimeMillis() - start) / 1000.;
+            double duration = (currentTimeMillis() - start) / 1000.;
             out("Single thread time, sec: %.3f", duration);
             singleThreadSum += duration;
 
-            start = System.currentTimeMillis();
-            final int[][] concurrentMatrixC = MatrixUtil.concurrentMultiply(matrixA, matrixB, executor);
-            duration = (System.currentTimeMillis() - start) / 1000.;
+            start = currentTimeMillis();
+            final int[][] singleThreadOptimizedMatrixC = MatrixUtil.singleThreadOptimizedMultiply(matrixA, matrixB);
+            duration = (currentTimeMillis() - start) / 1000.;
+            out("Single thread optimized time, sec: %.3f", duration);
+            singleThreadOptimizedSum += duration;
+
+            start = currentTimeMillis();
+            final int[][] concurrentMatrixC = MatrixUtil.concurrentMultiply(matrixA, matrixB, executor, optPageSize);
+            duration = (currentTimeMillis() - start) / 1000.;
             out("Concurrent thread time, sec: %.3f", duration);
             concurrentThreadSum += duration;
 
-            if (!MatrixUtil.compare(matrixC, concurrentMatrixC)) {
-                System.err.println("Comparison failed");
+            if (!MatrixUtil.compare(matrixC, singleThreadOptimizedMatrixC)) {
+                System.err.println("Comparison 1 failed");
                 break;
             }
-            count++;
+
+            if (!MatrixUtil.compare(matrixC, concurrentMatrixC)) {
+                System.err.println("Comparison 2 failed");
+                break;
+            }
         }
         executor.shutdown();
-        out("\nAverage single thread time, sec: %.3f", singleThreadSum / 5.);
-        out("Average concurrent thread time, sec: %.3f", concurrentThreadSum / 5.);
+        out("\nAverage single thread time, sec: %.3f", singleThreadSum / count);
+        out("Average single thread optimized time, sec: %.3f", singleThreadOptimizedSum / count);
+        out("Average concurrent thread time, sec: %.3f", concurrentThreadSum / count);
+    }
+
+    private int getOptPageSize(int[][] matrixA, int[][] matrixB) throws ExecutionException, InterruptedException {
+        double minDuration = Double.MAX_VALUE;
+
+        List<Info> infoList = new ArrayList<>();
+
+        for (int ps = 1; ps <= 100; ps++) {
+            System.out.println("Page size = " + ps);
+            long start = currentTimeMillis();
+            final int[][] matrixC = MatrixUtil.concurrentMultiply(matrixA, matrixB, executor, ps);
+            double duration = (currentTimeMillis() - start) / 1000.;
+            minDuration = Math.min(minDuration, duration);
+            //out("duration: %.3f", duration);
+            //out("minDuration: %.3f", minDuration);
+            infoList.add(new Info(duration, ps));
+        }
+
+        Collections.sort(infoList);
+
+        for (Info info : infoList) {
+            System.out.println(info);
+        }
+
+        return infoList.get(0).pageSize;
     }
 
     private static void out(String format, double ms) {
         System.out.println(String.format(format, ms));
+    }
+
+    private class Info implements Comparable<Info> {
+        private double duration;
+        private int pageSize;
+
+        public Info(double duration, int pageSize) {
+            this.duration = duration;
+            this.pageSize = pageSize;
+        }
+
+        @Override
+        public int compareTo(Info o) {
+            return Double.compare(duration, o.duration);
+        }
+
+        @Override
+        public String toString() {
+            final StringBuilder sb = new StringBuilder("Info{");
+            sb.append("duration=").append(duration);
+            sb.append(", pageSize=").append(pageSize);
+            sb.append('}');
+            return sb.toString();
+        }
     }
 }
--- src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
+++ src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
@@ -1,6 +1,9 @@
 package ru.javaops.masterjava.matrix;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 
@@ -10,28 +13,82 @@ import java.util.concurrent.ExecutorService;
  */
 public class MatrixUtil {
 
-    // TODO implement parallel multiplication matrixA*matrixB
-    public static int[][] concurrentMultiply(int[][] matrixA, int[][] matrixB, ExecutorService executor) throws InterruptedException, ExecutionException {
+    public static int[][] concurrentMultiply(int[][] matrixA, int[][] matrixB, ExecutorService executor, final int pageSize) throws InterruptedException, ExecutionException {
         final int matrixSize = matrixA.length;
         final int[][] matrixC = new int[matrixSize][matrixSize];
+
+        List<Callable<Object>> tasks = new ArrayList<>();
+
+        for (int n = 0; n < matrixSize; n += pageSize) {
+            int finalN = n;
+            tasks.add(() -> {
+                for (int k = 0; k < matrixSize; k++) {
+                    try {
+                        for (int i = finalN; i < finalN + pageSize; i++) {
+                            for (int j = 0; j < matrixSize; j++) {
+                                matrixC[i][j] += matrixA[i][k] * matrixB[k][j];
+                            }
+                        }
+                    } catch (IndexOutOfBoundsException ignored) {
+                    }
+                }
+                return null;
+            });
+        }
+
+        executor.invokeAll(tasks);
 
         return matrixC;
     }
 
-    // TODO optimize by https://habrahabr.ru/post/114797/
     public static int[][] singleThreadMultiply(int[][] matrixA, int[][] matrixB) {
         final int matrixSize = matrixA.length;
         final int[][] matrixC = new int[matrixSize][matrixSize];
 
-        for (int i = 0; i < matrixSize; i++) {
-            for (int j = 0; j < matrixSize; j++) {
-                int sum = 0;
-                for (int k = 0; k < matrixSize; k++) {
-                    sum += matrixA[i][k] * matrixB[k][j];
+//        for (int i = 0; i < matrixSize; i++) {
+//            for (int j = 0; j < matrixSize; j++) {
+//                int sum = 0;
+//                for (int k = 0; k < matrixSize; k++) {
+//                    sum += matrixA[i][k] * matrixB[k][j];
+//                }
+//                matrixC[i][j] = sum;
+//            }
+//        }
+
+        for (int k = 0; k < matrixSize; k++) {
+            for (int i = 0; i < matrixSize; i++) {
+                for (int j = 0; j < matrixSize; j++) {
+                    matrixC[i][j] += matrixA[i][k] * matrixB[k][j];
                 }
-                matrixC[i][j] = sum;
             }
         }
+        return matrixC;
+    }
+
+    public static int[][] singleThreadOptimizedMultiply(int[][] matrixA, int[][] matrixB) {
+        final int matrixSize = matrixA.length;
+        final int[][] matrixC = new int[matrixSize][matrixSize];
+
+        int col[] = new int[matrixSize];
+
+        try {
+            for (int j = 0; j < matrixSize; j++) {
+                for (int k = 0; k < matrixSize; k++) {
+                    col[k] = matrixB[k][j];
+                }
+
+                for (int i = 0; i < matrixSize; i++) {
+                    int row[] = matrixA[i];
+                    int sum = 0;
+                    for (int k = 0; k < matrixSize; k++) {
+                        sum += row[k] * col[k];
+                    }
+                    matrixC[i][j] = sum;
+                }
+            }
+        } catch (IndexOutOfBoundsException ignored) {
+        }
+
         return matrixC;
     }
 
