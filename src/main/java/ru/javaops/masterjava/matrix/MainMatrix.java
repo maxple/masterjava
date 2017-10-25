9ff6f19881f3b5f505d3d6362d7498443c295966
SBT-Pletnev-ML <MLPletnev.SBT@sberbank.ru>
2017-10-25 12:50:17 +0300

    hw1.2

--- src/main/java/ru/javaops/masterjava/matrix/MainMatrix.java
+++ src/main/java/ru/javaops/masterjava/matrix/MainMatrix.java
@@ -4,9 +4,39 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/*
+
+My results (4 cores):
+
+Pass 1
+Single thread time, sec: 1,092
+Concurrent thread time, sec: 0,452
+Concurrent maxple thread time, sec: 0,297
+Pass 2
+Single thread time, sec: 0,928
+Concurrent thread time, sec: 0,312
+Concurrent maxple thread time, sec: 0,203
+Pass 3
+Single thread time, sec: 0,873
+Concurrent thread time, sec: 0,312
+Concurrent maxple thread time, sec: 0,234
+Pass 4
+Single thread time, sec: 0,848
+Concurrent thread time, sec: 0,374
+Concurrent maxple thread time, sec: 0,203
+Pass 5
+Single thread time, sec: 0,862
+Concurrent thread time, sec: 0,302
+Concurrent maxple thread time, sec: 0,218
+
+Average single thread time, sec: 0,921
+Average concurrent thread time, sec: 0,350
+Average concurrent maxple thread time, sec: 0,231
+
+ */
 public class MainMatrix {
     private static final int MATRIX_SIZE = 1000;
-    private static final int THREAD_NUMBER = 10;
+    static final int THREAD_NUMBER = 10;
 
     private final static ExecutorService executor = Executors.newFixedThreadPool(MainMatrix.THREAD_NUMBER);
 
@@ -16,6 +46,7 @@ public class MainMatrix {
 
         double singleThreadSum = 0.;
         double concurrentThreadSum = 0.;
+        double concurrentMaxpleThreadSum = 0.;
         int count = 1;
         while (count < 6) {
             System.out.println("Pass " + count);
@@ -31,7 +62,13 @@ public class MainMatrix {
             out("Concurrent thread time, sec: %.3f", duration);
             concurrentThreadSum += duration;
 
-            if (!MatrixUtil.compare(matrixC, concurrentMatrixC)) {
+            start = System.currentTimeMillis();
+            final int[][] concurrentMaxpleMatrixC = MatrixUtil.concurrentMultiplyMaxple(matrixA, matrixB, executor);
+            duration = (System.currentTimeMillis() - start) / 1000.;
+            out("Concurrent maxple thread time, sec: %.3f", duration);
+            concurrentMaxpleThreadSum += duration;
+
+            if (!MatrixUtil.compare(matrixC, concurrentMatrixC) || !MatrixUtil.compare(matrixC, concurrentMaxpleMatrixC)) {
                 System.err.println("Comparison failed");
                 break;
             }
@@ -40,6 +77,7 @@ public class MainMatrix {
         executor.shutdown();
         out("\nAverage single thread time, sec: %.3f", singleThreadSum / 5.);
         out("Average concurrent thread time, sec: %.3f", concurrentThreadSum / 5.);
+        out("Average concurrent maxple thread time, sec: %.3f", concurrentMaxpleThreadSum / 5.);
     }
 
     private static void out(String format, double ms) {
--- src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
+++ src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
@@ -8,6 +8,36 @@ import java.util.stream.IntStream;
 
 public class MatrixUtil {
 
+    public static int[][] concurrentMultiplyMaxple(int[][] matrixA, int[][] matrixB, ExecutorService executor) throws InterruptedException, ExecutionException {
+        final int matrixSize = matrixA.length;
+        final int[][] matrixC = new int[matrixSize][matrixSize];
+
+        List<Callable<Object>> tasks = new ArrayList<>();
+
+        final int pageSize = matrixSize / MainMatrix.THREAD_NUMBER;
+
+        for (int n = 0; n < matrixSize; n += pageSize) {
+            int finalN = n;
+            tasks.add(() -> {
+                for (int k = 0; k < matrixSize; k++) {
+                    try {
+                        for (int row = finalN; row < finalN + pageSize; row++) {
+                            for (int col = 0; col < matrixSize; col++) {
+                                matrixC[row][col] += matrixA[row][k] * matrixB[k][col];
+                            }
+                        }
+                    } catch (IndexOutOfBoundsException ignored) {
+                    }
+                }
+                return null;
+            });
+        }
+
+        executor.invokeAll(tasks);
+
+        return matrixC;
+    }
+
     public static int[][] concurrentMultiplyStreams(int[][] matrixA, int[][] matrixB, int threadNumber)
             throws InterruptedException, ExecutionException {
 
