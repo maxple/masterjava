1a9dbdcd65eb66bdd0288dbe141462c25f822dd1
Pletnev-ML <MLPletnev.SBT@sberbank.ru>
2017-12-04 19:00:23 +0300

    hw7, fix + отправка писем

--- web/webapp/src/main/java/ru/javaops/masterjava/webapp/UsersServlet.java
+++ web/webapp/src/main/java/ru/javaops/masterjava/webapp/UsersServlet.java
@@ -4,6 +4,8 @@ import com.google.common.collect.ImmutableMap;
 import org.thymeleaf.context.WebContext;
 import ru.javaops.masterjava.persist.DBIProvider;
 import ru.javaops.masterjava.persist.dao.UserDao;
+import ru.javaops.masterjava.service.mail.Addressee;
+import ru.javaops.masterjava.service.mail.MailWSClient;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,6 +13,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.*;
 
 import static ru.javaops.masterjava.common.web.ThymeleafListener.engine;
 
@@ -23,5 +26,30 @@ public class UsersServlet extends HttpServlet {
         final WebContext webContext = new WebContext(req, resp, req.getServletContext(), req.getLocale(),
                 ImmutableMap.of("users", userDao.getWithLimit(20)));
         engine.process("users", webContext, resp.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String subject = "";
+        String body = "";
+        Set<Addressee> addresseeSet = new HashSet<>();
+
+        for (String s : Collections.list(req.getParameterNames())) {
+            switch (s) {
+                case "subject":
+                    subject = req.getParameterValues(s)[0];
+                    break;
+                case "body":
+                    body = req.getParameterValues(s)[0];
+                    break;
+                default:
+                    addresseeSet.add(new Addressee(s));
+                    break;
+            }
+        }
+
+        if (addresseeSet.size() < 1) return;
+
+        MailWSClient.sendToGroup(addresseeSet, new HashSet<>(), subject, body);
     }
 }
